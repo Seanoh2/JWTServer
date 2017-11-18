80be412f79c8abb92fe1947df7100fe9ccf13414
Sean O Hora <D00186551@student.dkit.ie>
2017-11-18 19:08:15 +0000

    base

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'JWTClass.php';
+require_once 'JWT_class.php';
 $secret = "Peter's Key";
 $api_key = filter_input(INPUT_GET, 'api_key');
 $Service = filter_input(INPUT_GET, 'Service');
@@ -23,6 +23,33 @@ switch ($Service) :
         break;
     
     case 'Service1' :
+        //Check the api key.
+        $api_key  = filter_input(INPUT_GET, 'api_key');
+        try{
+            $token = JWT::decode($api_key, $secret);
+        } catch (ErrorException $ex) {
+            return -1;
+        }
+        echo "This service is free to " . $token->username;
+        
+        break;
     
-    
+    case 'Service2' :
+        //Check the api key.
+        $api_key  = filter_input(INPUT_GET, 'api_key');
+        try{
+            $token = JWT::decode($api_key, $secret);
+        } catch (ErrorException $ex) {
+            return -1;
+        }
+        if($token->memType == "Paid") {
+            echo "Thank you for buying our service " . $token->username . ".";
+        } else {
+            echo "Sorry, this service is only for paid members.";
+        }
+        break;
+        
+    default:
+        echo "ERROR";
+        break;
 endswitch;
